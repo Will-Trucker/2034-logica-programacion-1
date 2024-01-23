7669f27a943afd1914384ec111ae5890e97d3011
Will-Trucker <willitomarley@outlook.es>
2024-01-22 23:16:20 -0600

    Variables y Condiciones If

---
+++ app.js
@@ -0,0 +1,8 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un número por favor: "); // Aqui queremos mandar un parametro al cliente
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto){
+ alert('Acertaste el número');
+}
