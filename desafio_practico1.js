19c2a3fe484e5d408ad7aead36f3dc52beb492c6
Will-Trucker <willitomarley@outlook.es>
2024-01-23 16:35:06 -0600

    Desafio 1

---
+++ desafio_practico1.js
@@ -0,0 +1,23 @@
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
+
+//let nombre = Luna;
+
+//let edad = 25;
+
+let numeroDeVentas = 50;
+
+let saldoDisponible = 1000;
+
+alert('¡Error! Completa todos los campos');
+
+let mensajeDeError = "¡Error! Completa todos los campos"
+
+alert(mensajeDeError);
+
+let nombre = prompt('Digite su nombre');
+
+let edad = prompt('Digite su edad');
+
+if(edad >= 18){
+    alert('Puedes obtener una licencia de conducir');
+}
