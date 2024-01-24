3d06fe6c54a70c21523e81e6ec29e48b9686a559
Will-Trucker <willitomarley@outlook.es>
2024-01-24 16:32:44 -0600

    Break

--- 2034-logica-programacion-1-Aula2/app.js
+++ 2034-logica-programacion-1-Aula2/app.js
@@ -1,20 +1,29 @@
 //Variables
 let numeroSecreto = 4;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 while (numeroUsuario != numeroSecreto) {
 
-prompt("Me indicas un número entre 1 y 10 por favor:");
+numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
 
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condicion
-        alert(`Acertaste, el número es: ${numeroUsuario}`); // Template Strings
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`); // Template Strings
     } else {
         if (numeroUsuario > numeroSecreto){
             alert('El número secreto es menor');
         } else {
             alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        if (intentos > 3) {
+            alert('Llegaste al número maximo de intentos');
+            break;
         }
         //La condicion, no se cumplio
         //alert('Lo siento no acertaste el numero');
